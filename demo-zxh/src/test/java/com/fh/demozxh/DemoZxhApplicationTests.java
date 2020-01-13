85f264c13d632a9e07ba85217d767576a4909228
zhangxuhao <212145091@qq.com>
2020-01-13 10:22:09 +0800

    恢复

---
+++ demo-zxh/src/test/java/com/fh/demozxh/DemoZxhApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fh.demozxh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoZxhApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
